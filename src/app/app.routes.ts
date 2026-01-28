9b6bd70c512507d35efc8e2a3cb6cc0ceed65a91
Rémi Campistron <remi.campistron@gmail.com>
2026-01-28 14:59:52 +0100

    maj des routes pour liste client et ajouter client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {Trainings} from './components/trainings/trainings';
 import {Cart} from './components/cart/cart';
+import {CustomerComponent} from './components/customer/customer';
+import {CustomerList} from './components/customer-list/customer-list';
 
 export const routes: Routes = [
   { path: 'trainings', component: Trainings},
-  { path: 'cart',component: Cart}
+  { path: 'cart',component: Cart},
+  { path: 'customerList', component: CustomerList},
+  { path: 'customer', component: CustomerComponent}
 ];
 
 @NgModule({
