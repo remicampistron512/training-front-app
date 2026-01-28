e0428a182cb3611c3f72700cc335ea5b524341c1
Rémi Campistron <remi.campistron@gmail.com>
2026-01-28 15:03:18 +0100

    refactor du composant cart

--- src/app/components/cart/cart.ts
+++ src/app/components/cart/cart.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CartService } from '../../services/cart.service';   // adjust
+import { Training } from '../../model/training/training.model';      // adjust
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
 export class Cart {
+  constructor(public cartService: CartService) {}
 
+  get trainings(): Training[] {
+    return this.cartService.getTrainings();
+  }
+
+  removeFromCart(t: Training): void {
+    this.cartService.removeTraining(t);
+  }
 }
