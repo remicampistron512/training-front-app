6bba6dac545e716b9f53321aec1ded3036a1d0a8
Rémi Campistron <remi.campistron@gmail.com>
2026-01-28 15:00:46 +0100

    ajout et recuperation d'une formation dans le cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
-import {Training} from '../model/training.models';
+import { Training } from '../model/training/training.model';
+import { CartModel } from '../model/cart/cart.model';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class CartService {
-  listTrainings: Training[] = [];
-  addTraining (training: Training){
-    this.listTrainings.push(training);
+  cart: CartModel = new CartModel();
+
+  addTraining(training: Training): void {
+    this.cart.listTrainings.push(training);
+  }
+
+  getTrainings(): Training[] {
+    return this.cart.listTrainings;
+  }
+
+  removeTraining(training: Training): void {
+    const idx = this.cart.listTrainings.findIndex(t => t.id === training.id);
+    if (idx >= 0) this.cart.listTrainings.splice(idx, 1);
   }
 }
